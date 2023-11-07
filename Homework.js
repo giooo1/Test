d5941ff0aed5240dae84d5ee23e38c0cac335b9a
giooo1 <giorgigakharia@gmail.com>
2023-11-07 16:15:36 +0400

    Homework

--- Homework.js
+++ Homework.js
@@ -1,3 +1,51 @@
-let a = 87;
-let b = 65;
+var a = 87;
+var b = 65;
 console.log(a+b);
+
+let phones = ["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
+console.log(phones);
+
+phones[4] = "Oneplus";
+console.log(phones);
+
+let student = {
+    firstname: "giorgi",
+    lastname: "gakharia",
+    age: 26 , 
+    email: "giorgigakharia@gmail.com",
+    marks: 10,
+
+}
+console.log(student.firstname);
+
+
+let m = 45;
+let n = 23;
+if (m>n) {
+    console.log(m+n);
+} else {
+    console.log(n)
+}
+
+
+let studentname = "giorgi";
+console.log("Rise And Shine" + " " + studentname + " " + "!");
+
+
+let bonus = 0;
+let salary = 1000;
+if (salary >= 2000 ) {
+    console.log(bonus);
+} else if (salary < 2000) {
+    console.log(bonus= salary/10);
+}
+
+var a = 23;
+var b = (a/2);
+var c = (a+b);
+console.log(c);
+
+
+let str1 = "გიორგი გახარია";
+let str2 = "სამშაბათი";
+console.log("მოგესალმებით" + " " + str1 + " " + "და დღეს" + " " + str2 + "ა");
